dcc908fd7f24fbe6850c49723afaea33eb453dce
mjy34 <mjy34@dcgworld.com>
2017-08-03 11:45:43 +0900

    -a

--- webpack_기존쓰던파일/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(<App />, rootElement);
-
